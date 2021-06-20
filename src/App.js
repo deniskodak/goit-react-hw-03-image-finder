b1dedb654edf7c8021c8f4c01c5667ed4f94169f
deniskodak <denis.kodak@gmail.com>
2021-06-20 14:59:51 +0300

    make a searchbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Searchbar from './components/Searchbar';
-
-
+import Searchbar from "./components/Searchbar";
+const API_KEY = "21310703-eb2542faa873a37e647429bbc";
 class App extends Component {
-  render() {
-    <Searchbar/>
+  state = {
+    keyword: " ",
+    BASE_URL: `https://pixabay.com/api/?q=${this.state.keyword}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+  };
 
+  componentDidMount() {
+    if (this.state.keyword) {
+      fetch(this.state.BASE_URL)
+        .then((respose) => respose.json())
+        .then((data) => console.log(data));
+    }
   }
-};
+
+  handleSubmit = (keyword) => {
+    this.state({ keyword: keyword });
+  };
+  render() {
+    return <Searchbar onSubmit={this.handleSubmit} />;
+  }
+}
 
 export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,21 +1,38 @@
+import React, { Component } from "react";
 
-const Searchbar = () => {
+class Searchbar extends Component {
+  state = {
+    value: "",
+  };
 
+  handleChange = ({ target: { value } }) => {
+    this.setState({ value: value });
+  };
+
+  render() {
     return (
-    <header className="Searchbar">
-        <form className="SearchForm">
-            <button type="submit" className="SearchForm-button">
+      <header className="Searchbar">
+        <form
+          onSubmit={this.props.onSubmit(this.state.value)}
+          className="SearchForm"
+        >
+          <button type="submit" className="SearchForm-button">
             <span className="SearchForm-button-label">Search</span>
-            </button>
-  
-            <input
+          </button>
+
+          <input
             className="SearchForm-input"
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
+            value={this.state.value}
+            onChange={this.handleChange}
             placeholder="Search images and photos"
-            />
-       </form>
-    </header>
-)
+          />
+        </form>
+      </header>
+    );
+  }
 }
+
+export default Searchbar;
